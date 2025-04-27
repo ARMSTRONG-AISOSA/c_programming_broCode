81ab5a28c38d8cb451c9f513546d202bcd7cfa64
Armstrong <ammycan6@gmail.com>
2025-04-27 15:37:59 +0100

    first commit on my linux mint pc, learning C

---
+++ HelloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// <stdio.h>: contains useful function related to input and output
+// std: standard
+// i: input
+// 0: output
+
+// Aynting in our "main" function is read procedurally, i.e top to bottom.
+int main() {
+    
+}
