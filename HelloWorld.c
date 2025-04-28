3ab1c4169573853c13a5949aea85ae06a373d8ef
Armstrong <ammycan6@gmail.com>
2025-04-28 12:01:51 +0100

    Lesson 6: Format Specifiers

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,6 +3,7 @@
 
 void main01()
 {
+    printf("\nFunction main01\n\n");
     printf("I like pizza!\n");
     printf("\"It's really good shampoo\" -Jacob Thadeus\n");
     printf("\\Print backslashes\\\n");
@@ -11,6 +12,7 @@ void main01()
 void main02()
 {
     // txt --> 2.0
+    printf("\nFunction main02\n\n");
     printf("1\n2\n3\n");
     printf("1tab\t2tab\t3tab\n");
     printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
@@ -32,6 +34,7 @@ void main03()
     signed long long int lgLgInt = -1234567890987654321;
     long long int uLgLgInt = 1234567890987654321;
 
+    printf("\nFunction main03\n\n");
     printf("Hello %s\n", name); // txt --> 5.2
     printf("I am %d years old.\n", age);
     printf("Her English grade is %c.\n", grade);
@@ -44,10 +47,23 @@ void main03()
     printf("The value of the unsigned long long integer %llu.\n", uLgLgInt);
 }
 
+void main04()
+{
+    float item1 = 5.75;
+    float item2 = 10.75;
+    float item3 = 200.75;
+
+    printf("\nFunction main04\n\n");
+    printf("Item 1: $%.2f.\n", item1);
+    printf("Item 1: $%.2f, Item 2: $%.2f, Item 3: $%.2f.\n", item1, item2, item3);
+}
+
+
 int main()
 {
     main01();
     main02();
     main03();
+    main04();
     return 0; // txt --> 3.1
 }
