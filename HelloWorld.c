e16e2c98c29182599c95ce65d421e709919af644
Armstrong <ammycan6@gmail.com>
2025-04-28 11:42:12 +0100

    Lesson 5: Data types. Ps:Lesson 4 was actually varaibles

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,42 +1,53 @@
 #include <stdio.h> //txt --> 1.0
+#include <stdbool.h>
 
-
-void main01() {
+void main01()
+{
     printf("I like pizza!\n");
     printf("\"It's really good shampoo\" -Jacob Thadeus\n");
     printf("\\Print backslashes\\\n");
 }
 
-void main02() {
-    //txt --> 2.0
+void main02()
+{
+    // txt --> 2.0
     printf("1\n2\n3\n");
     printf("1tab\t2tab\t3tab\n");
     printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
 }
 
-
 // === Lesson 4 Variables
+void main03()
+{
+    // txt --> 4.0 & 5.0
+    // ps: signed => store neg. values
+    int age = 23;
+    float gpa = 3.02;
+    double pie = 3.14159265358979323846;
+    char grade = 'c';
+    char name[] = "Benson"; // txt --> 5.1
+    bool t = true;
+    bool f = false;
+    short int stInt = 32767;
+    signed long long int lgLgInt = -1234567890987654321;
+    long long int uLgLgInt = 1234567890987654321;
 
-void main03() {
-//txt --> 4.0
-int age = 23;
-float gpa = 3.02; 
-char grade = 'c';
-char name[] = "Benson";
-
-printf("Hello %s\n", name);
-printf("I am %d years old.\n",age);
-printf("Her English grade is %c.\n", grade);
-printf("His gpa is %f.\n", gpa);
+    printf("Hello %s\n", name); // txt --> 5.2
+    printf("I am %d years old.\n", age);
+    printf("Her English grade is %c.\n", grade);
+    printf("His gpa is %f.\n", gpa);
+    printf("The value of pie printed with a double and long float %f.\n", pie);
+    printf("The value of pie printed with a double, long float and 15 specified characters %0.15lf.\n", pie);
+    printf("The boolean value is %d.\n", f);
+    printf("The value of the short integer %d.\n", stInt);
+    printf("The value of the long long integer %lld.\n", lgLgInt);
+    printf("The value of the unsigned long long integer %llu.\n", uLgLgInt);
 }
 
-
-
-
-
-int main() {
+int main()
+{
     main01();
     main02();
     main03();
-    return 0; //txt --> 3.1
+    return 0; // txt --> 3.1
 }
